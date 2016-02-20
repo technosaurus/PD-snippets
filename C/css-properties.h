890b158af5cdab697229ecc1dd10cea069a3234e
technosaurus <technosaurus@yahoo.com>
2016-02-20 04:08:00 -0600

    more CSS properties
    Currently I am adding browser extensions to but will remove some once I get to 256 actual properties.
    This is useful for storing the type using 1 byte of a struct.
    The plan is to remap duplicates first, then remove the most useless/deprecated ones.
    If it gets past that, some of the speech and print tags can be moved out.

--- C/css-properties.h
+++ C/css-properties.h
@@ -1,11 +1,36 @@
 //https://www.w3.org/TR/CSS2/propidx.html
 #define CSS_PROP_MAP { \
+	_MAP(_MOZ_BINDING,          "-moz-binding"), \
+	_MAP(_MOZ_BORDER_RADIUS,    "-moz-border-radius"), \
+	_MAP(_MOZ_BORDER_RADIUS_BOTTOMLEFT,"-moz-border-radius-bottomleft"), \
+	_MAP(_MOZ_BORDER_RADIUS_BOTTOMRIGHT,"-moz-border-radius-bottomright"), \
+	_MAP(_MOZ_BORDER_RADIUS_TOPLEFT,"-moz-border-radius-topleft"), \
+	_MAP(_MOZ_BORDER_RADIUS_TOPRIGHT,"-moz-border-radius-topright"), \
+	_MAP(_MOZ_BORDER_BOTTOM_COLORS,"-moz-border-bottom-colors"), \
+	_MAP(_MOZ_BORDER_LEFT_COLORS,"-moz-border-left-colors"), \
+	_MAP(_MOZ_BORDER_RIGHT_COLORS,"-moz-border-right-colors"), \
+	_MAP(_MOZ_BORDER_TOP_COLORS,"-moz-border-top-colors"), \
+	_MAP(_MOZ_OPACITY,          "-moz-opacity"), \
+	_MAP(_MOZ_OUTLINE,          "-moz-outline"), \
+	_MAP(_MOZ_OUTLINE_COLOR,    "-moz-outline-color"), \
+	_MAP(_MOZ_OUTLINE_STYLE,    "-moz-outline-style"), \
+	_MAP(_MOZ_OUTLINE_WIDTH,    "-moz-outline-width"), \
+	_MAP(_MOZ_USER_FOCUS,       "-moz-user-focus"), \
+	_MAP(_MOZ_USER_INPUT,       "-moz-user-input"), \
+	_MAP(_MOZ_USER_MODIFY,      "-moz-user-modify"), \
+	_MAP(_MOZ_USER_SELECT,      "-moz-user-select"), \
+	_MAP(_O_REPLACE,            "-o-replace"), \
+	_MAP(_O_SET_LINK_SOURCE,    "-o-set-link-source"), \
+	_MAP(_O_USE_LINK_SOURCE,    "-o-use-link-source"), \
+	_MAP(ACCELERATOR,           "accelerator"), \
 	_MAP(AZIMUTH,               "azimuth"), \
 	_MAP(BACKGROUND,            "background"), \
 	_MAP(BACKGROUND_ATTACHMENT, "background-attachment"), \
 	_MAP(BACKGROUND_COLOR,      "background-color"), \
 	_MAP(BACKGROUND_IMAGE,      "background-image"), \
 	_MAP(BACKGROUND_POSITION,   "background-position"), \
+	_MAP(BACKGROUND_POSITION_X, "background-position-x"), \
+	_MAP(BACKGROUND_POSITION_Y, "background-position-y"), \
 	_MAP(BACKGROUND_REPEAT,     "background-repeat"), \
 	_MAP(BORDER,                "border"), \
 	_MAP(BORDER_BOTTOM,         "border-bottom"), \
@@ -45,16 +70,31 @@
 	_MAP(DISPLAY,               "display"), \
 	_MAP(ELEVATION,             "elevation"), \
 	_MAP(EMPTY_CELLS,           "empty-cells"), \
+	_MAP(FILTER,                "filter"), \
 	_MAP(FLOAT,                 "float"), \
 	_MAP(FONT,                  "font"), \
 	_MAP(FONT_FAMILY,           "font-family"), \
 	_MAP(FONT_SIZE,             "font-size"), \
+	_MAP(FONT_SIZE_ADJUST,      "font-size-adjust"), \
+	_MAP(FONT_STRETCH,          "font-stretch"), \
 	_MAP(FONT_STYLE,            "font-style"), \
 	_MAP(FONT_VARIANT,          "font-variant"), \
 	_MAP(FONT_WEIGHT,           "font-weight"), \
 	_MAP(HEIGHT,                "height"), \
+	_MAP(IME_MODE,              "ime-mode"), \
+	_MAP(INCLUDE_SOURCE,        "include-source"), \
+	_MAP(LAYER_BACKGROUND_COLOR,"layer-background-color"), \
+	_MAP(LAYER_BACKGROUND_IMAGE,"layer-background-image"), \
+	_MAP(LAYOUT_FLOW,           "layout-flow"), \
+	_MAP(LAYOUT_GRID,           "layout-grid"), \
+	_MAP(LAYOUT_GRID_CHAR,      "layout-grid-char"), \
+	_MAP(LAYOUT_GRID_CHAR_SPACING,"layout-grid-char-spacing"), \
+	_MAP(LAYOUT_GRID_LINE,      "layout-grid-line"), \
+	_MAP(LAYOUT_GRID_MODE,      "layout-grid-mode"), \
+	_MAP(LAYOUT_GRID_TYPE,      "layout-grid-type"), \
 	_MAP(LEFT,                  "left"), \
 	_MAP(LETTER_SPACING,        "letter-spacing"), \
+	_MAP(LINE_BREAK,            "line-break"), \
 	_MAP(LINE_HEIGHT,           "line-height"), \
 	_MAP(LIST_STYLE,            "list-style"), \
 	_MAP(LIST_STYLE_IMAGE,      "list-style-image"), \
@@ -65,6 +105,8 @@
 	_MAP(MARGIN_LEFT,           "margin-left"), \
 	_MAP(MARGIN_RIGHT,          "margin-right"), \
 	_MAP(MARGIN_TOP,            "margin-top"), \
+	_MAP(MARKER_OFFSET,         "marker-offset"), \
+	_MAP(MARKS,                 "marks"), \
 	_MAP(MAX_HEIGHT,            "max-height"), \
 	_MAP(MAX_WIDTH,             "max-width"), \
 	_MAP(MIN_HEIGHT,            "min-height"), \
@@ -75,11 +117,14 @@
 	_MAP(OUTLINE_STYLE,         "outline-style"), \
 	_MAP(OUTLINE_WIDTH,         "outline-width"), \
 	_MAP(OVERFLOW,              "overflow"), \
+	_MAP(OVERFLOW_X,            "overflow-x"), \
+	_MAP(OVERFLOW_X,            "overflow-y"), \
 	_MAP(PADDING,               "padding"), \
 	_MAP(PADDING_BOTTOM,        "padding-bottom"), \
 	_MAP(PADDING_LEFT,          "padding-left"), \
 	_MAP(PADDING_RIGHT,         "padding-right"), \
 	_MAP(PADDING_TOP,           "padding-top"), \
+	_MAP(PAGE,                  "page"), \
 	_MAP(PAGE_BREAK_AFTER,      "page-break-after"), \
 	_MAP(PAGE_BREAK_BEFORE,     "page-break-before"), \
 	_MAP(PAGE_BREAK_INSIDE,     "page-break-inside"), \
@@ -93,6 +138,18 @@
 	_MAP(QUOTES,                "quotes"), \
 	_MAP(RICHNESS,              "richness"), \
 	_MAP(RIGHT,                 "right"), \
+	_MAP(RUBY_ALIGN,            "ruby-align"), \
+	_MAP(RUBY_OVERHANG,         "ruby-overhang"), \
+	_MAP(RUBY_POSITION,         "ruby-position"), \
+	_MAP(SCROLLBAR_ARROW_COLOR, "scrollbar-arrow-color"), \
+	_MAP(SCROLLBAR_BASE_COLOR,  "scrollbar-base-color"), \
+	_MAP(SCROLLBAR_DARK_SHADOW_COLOR,"scrollbar-dark-shadow-color"), \
+	_MAP(SCROLLBAR_FACE_COLOR,  "scrollbar-face-color"), \
+	_MAP(SCROLLBAR_HIGHLIGHT_COLOR,"scrollbar-highlight-color"), \
+	_MAP(SCROLLBAR_SHADOW_COLOR,"scrollbar-shadow-color"), \
+	_MAP(SCROLLBAR_3D_LIGHT_COLOR,"scrollbar-3d-light-color"), \
+	_MAP(SCROLLBAR_TRACK_COLOR, "scrollbar-track-color"), \
+	_MAP(SIZE,                  "size"), \
 	_MAP(SPEAK,                 "speak"), \
 	_MAP(SPEAK_HEADER,          "speak-header"), \
 	_MAP(SPEAK_NUMERAL,         "speak-numeral"), \
@@ -101,9 +158,16 @@
 	_MAP(STRESS,                "stress"), \
 	_MAP(TABLE_LAYOUT,          "table-layout"), \
 	_MAP(TEXT_ALIGN,            "text-align"), \
+	_MAP(TEXT_ALIGN_LAST,       "text-align-last"), \
+	_MAP(TEXT_AUTOSPACE,        "text-autospace"), \
 	_MAP(TEXT_DECORATION,       "text-decoration"), \
 	_MAP(TEXT_INDENT,           "text-indent"), \
+	_MAP(TEXT_JUSTIFY           "text-justify"), \
+	_MAP(TEXT_KASHIDA_SPACE,    "text-kashida-space"), \
+	_MAP(TEXT_OVERFLOW,         "text-overflow"), \
+	_MAP(TEXT_SHADOW,           "text-shadow"), \
 	_MAP(TEXT_TRANSFORM,        "text-transform"), \
+	_MAP(TEXT_UNDERLINE_POSITION,"text-underline-position"), \
 	_MAP(TOP,                   "top"), \
 	_MAP(UNICODE_BIDI,          "unicode-bidi"), \
 	_MAP(VERTICAL_ALIGN,        "vertical-align"), \
@@ -113,6 +177,10 @@
 	_MAP(WHITE_SPACE,           "white-space"), \
 	_MAP(WIDOWS,                "widows"), \
 	_MAP(WIDTH,                 "width"), \
+	_MAP(WORD_BREAK,            "word-break"), \
 	_MAP(WORD_SPACING,          "word-spacing"), \
+	_MAP(WORD_WRAP,             "word-wrap"), \
+	_MAP(WRITING_MODE,          "writing-mode"), \
 	_MAP(Z_INDEX,               "z-index"),\
+	_MAP(ZOOM,                  "zoom"),\
 }
