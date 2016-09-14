a16063bc18f10427c749aec7730201d8e4fb6bbb
technosaurus <technosaurus@yahoo.com>
2016-09-14 03:56:27 -0500

    Update css-property-values.h

--- C/css-property-values.h
+++ C/css-property-values.h
@@ -1,161 +1,160 @@
 //https://www.w3.org/TR/CSS2/propidx.html
-#define CSS_PROP_VAL_MAP { \
-	MAP(CSS_PROP_VAL_100,                  "100"), \
-	MAP(CSS_PROP_VAL_200,                  "200"), \
-	MAP(CSS_PROP_VAL_300,                  "300"), \
-	MAP(CSS_PROP_VAL_400,                  "400"), \
-	MAP(CSS_PROP_VAL_500,                  "500"), \
-	MAP(CSS_PROP_VAL_600,                  "600"), \
-	MAP(CSS_PROP_VAL_700,                  "700"), \
-	MAP(CSS_PROP_VAL_800,                  "800"), \
-	MAP(CSS_PROP_VAL_900,                  "900"), \
-	MAP(CSS_PROP_VAL_ABOVE,                "above"), \
-	MAP(CSS_PROP_VAL_ABSOLUTE,             "absolute"), \
-	MAP(CSS_PROP_VAL_ALWAYS,               "always"), \
-	MAP(CSS_PROP_VAL_ARMENIAN,             "armenian"), \
-	MAP(CSS_PROP_VAL_AUTO,                 "auto"), \
-	MAP(CSS_PROP_VAL_AVOID,                "avoid"), \
-	MAP(CSS_PROP_VAL_BASELINE,             "baseline"), \
-	MAP(CSS_PROP_VAL_BEHIND,               "behind"), \
-	MAP(CSS_PROP_VAL_BELOW,                "below"), \
-	MAP(CSS_PROP_VAL_BIDI_OVERRIDE,        "bidi-override"), \
-	MAP(CSS_PROP_VAL_BLINK,                "blink"), \
-	MAP(CSS_PROP_VAL_BLOCK,                "block"), \
-	MAP(CSS_PROP_VAL_BOLD,                 "bold"), \
-	MAP(CSS_PROP_VAL_BOLDER,               "bolder"), \
-	MAP(CSS_PROP_VAL_BOTH,                 "both"), \
-	MAP(CSS_PROP_VAL_BOTTOM,               "bottom"), \
-	MAP(CSS_PROP_VAL_CAPITALIZE,           "capitalize"), \
-	MAP(CSS_PROP_VAL_CAPTION,              "caption"), \
-	MAP(CSS_PROP_VAL_CENTER,               "center"), \
-	MAP(CSS_PROP_VAL_CENTER_LEFT,          "center-left"), \
-	MAP(CSS_PROP_VAL_CENTER_RIGHT,         "center-right"), \
-	MAP(CSS_PROP_VAL_CIRCLE,               "circle"), \
-	MAP(CSS_PROP_VAL_CLOSE_QUOTE,          "close-quote"), \
-	MAP(CSS_PROP_VAL_CODE,                 "code"), \
-	MAP(CSS_PROP_VAL_COLLAPSE,             "collapse"), \
-	MAP(CSS_PROP_VAL_CONTINUOUS,           "continuous"), \
-	MAP(CSS_PROP_VAL_CROSSHAIR,            "crosshair"), \
-	MAP(CSS_PROP_VAL_DECIMAL,              "decimal"), \
-	MAP(CSS_PROP_VAL_DECIMAL_LEADING_ZERO, "decimal-leading-zero"), \
-	MAP(CSS_PROP_VAL_DEFAULT,              "default"), \
-	MAP(CSS_PROP_VAL_DIGITS,               "digits"), \
-	MAP(CSS_PROP_VAL_DISC,                 "disc"), \
-	MAP(CSS_PROP_VAL_EMBED,                "embed"), \
-	MAP(CSS_PROP_VAL_E_RESIZE,             "e-resize"), \
-	MAP(CSS_PROP_VAL_FAR_LEFT,             "far-left"), \
-	MAP(CSS_PROP_VAL_FAR_RIGHT,            "far-right"), \
-	MAP(CSS_PROP_VAL_FAST,                 "fast"), \
-	MAP(CSS_PROP_VAL_FASTER,               "faster"), \
-	MAP(CSS_PROP_VAL_FIXED,                "fixed"), \
-	MAP(CSS_PROP_VAL_GEORGIAN,             "georgian"), \
-	MAP(CSS_PROP_VAL_HELP,                 "help"), \
-	MAP(CSS_PROP_VAL_HIDDEN,               "hidden"), \
-	MAP(CSS_PROP_VAL_HIDE,                 "hide"), \
-	MAP(CSS_PROP_VAL_HIGH,                 "high"), \
-	MAP(CSS_PROP_VAL_HIGHER,               "higher"), \
-	MAP(CSS_PROP_VAL_ICON,                 "icon"), \
-	MAP(CSS_PROP_VAL_INHERIT,              "inherit"), \
-	MAP(CSS_PROP_VAL_INLINE,               "inline"), \
-	MAP(CSS_PROP_VAL_INLINE_BLOCK,         "inline-block"), \
-	MAP(CSS_PROP_VAL_INLINE_TABLE,         "inline-table"), \
-	MAP(CSS_PROP_VAL_INSIDE,               "inside"), \
-	MAP(CSS_PROP_VAL_INVERT,               "invert"), \
-	MAP(CSS_PROP_VAL_ITALIC,               "italic"), \
-	MAP(CSS_PROP_VAL_JUSTIFY,              "justify"), \
-	MAP(CSS_PROP_VAL_LEFT,                 "left"), \
-	MAP(CSS_PROP_VAL_LEFT_SIDE,            "left-side"), \
-	MAP(CSS_PROP_VAL_LEFTWARDS,            "leftwards"), \
-	MAP(CSS_PROP_VAL_LEVEL,                "level"), \
-	MAP(CSS_PROP_VAL_LIGHTER,              "lighter"), \
-	MAP(CSS_PROP_VAL_LINE_THROUGH,         "line-through"), \
-	MAP(CSS_PROP_VAL_LIST_ITEM,            "list-item"), \
-	MAP(CSS_PROP_VAL_LOUD,                 "loud"), \
-	MAP(CSS_PROP_VAL_LOW,                  "low"), \
-	MAP(CSS_PROP_VAL_LOWER,                "lower"), \
-	MAP(CSS_PROP_VAL_LOWER_ALPHA,          "lower-alpha"), \
-	MAP(CSS_PROP_VAL_LOWERCASE,            "lowercase"), \
-	MAP(CSS_PROP_VAL_LOWER_GREEK,          "lower-greek"), \
-	MAP(CSS_PROP_VAL_LOWER_LATIN,          "lower-latin"), \
-	MAP(CSS_PROP_VAL_LOWER_ROMAN,          "lower-roman"), \
-	MAP(CSS_PROP_VAL_LTR,                  "ltr"), \
-	MAP(CSS_PROP_VAL_MEDIUM,               "medium"), \
-	MAP(CSS_PROP_VAL_MENU,                 "menu"), \
-	MAP(CSS_PROP_VAL_MESSAGE_BOX,          "message-box"), \
-	MAP(CSS_PROP_VAL_MIDDLE,               "middle"), \
-	MAP(CSS_PROP_VAL_MIX,                  "mix"), \
-	MAP(CSS_PROP_VAL_MOVE,                 "move"), \
-	MAP(CSS_PROP_VAL_NE_RESIZE,            "ne-resize"), \
-	MAP(CSS_PROP_VAL_NO_CLOSE_QUOTE,       "no-close-quote"), \
-	MAP(CSS_PROP_VAL_NONE,                 "none"), \
-	MAP(CSS_PROP_VAL_NO_OPEN_QUOTE,        "no-open-quote"), \
-	MAP(CSS_PROP_VAL_NO_REPEAT,            "no-repeat"), \
-	MAP(CSS_PROP_VAL_NORMAL,               "normal"), \
-	MAP(CSS_PROP_VAL_NOWRAP,               "nowrap"), \
-	MAP(CSS_PROP_VAL_N_RESIZE,             "n-resize"), \
-	MAP(CSS_PROP_VAL_NW_RESIZE,            "nw-resize"), \
-	MAP(CSS_PROP_VAL_OBLIQUE,              "oblique"), \
-	MAP(CSS_PROP_VAL_ONCE,                 "once"), \
-	MAP(CSS_PROP_VAL_OPEN_QUOTE,           "open-quote"), \
-	MAP(CSS_PROP_VAL_OUTSIDE,              "outside"), \
-	MAP(CSS_PROP_VAL_OVERLINE,             "overline"), \
-	MAP(CSS_PROP_VAL_POINTER,              "pointer"), \
-	MAP(CSS_PROP_VAL_PRE,                  "pre"), \
-	MAP(CSS_PROP_VAL_PRE_LINE,             "pre-line"), \
-	MAP(CSS_PROP_VAL_PRE_WRAP,             "pre-wrap"), \
-	MAP(CSS_PROP_VAL_PROGRESS,             "progress"), \
-	MAP(CSS_PROP_VAL_RELATIVE,             "relative"), \
-	MAP(CSS_PROP_VAL_REPEAT,               "repeat"), \
-	MAP(CSS_PROP_VAL_REPEAT_X,             "repeat-x"), \
-	MAP(CSS_PROP_VAL_REPEAT_Y,             "repeat-y"), \
-	MAP(CSS_PROP_VAL_RIGHT,                "right"), \
-	MAP(CSS_PROP_VAL_RIGHT_SIDE,           "right-side"), \
-	MAP(CSS_PROP_VAL_RIGHTWARDS,           "rightwards"), \
-	MAP(CSS_PROP_VAL_RTL,                  "rtl"), \
-	MAP(CSS_PROP_VAL_SCROLL,               "scroll"), \
-	MAP(CSS_PROP_VAL_SEPARATE,             "separate"), \
-	MAP(CSS_PROP_VAL_SE_RESIZE,            "se-resize"), \
-	MAP(CSS_PROP_VAL_SHOW,                 "show"), \
-	MAP(CSS_PROP_VAL_SILENT,               "silent"), \
-	MAP(CSS_PROP_VAL_SLOW,                 "slow"), \
-	MAP(CSS_PROP_VAL_SLOWER,               "slower"), \
-	MAP(CSS_PROP_VAL_SMALL_CAPS,           "small-caps"), \
-	MAP(CSS_PROP_VAL_SMALL_CAPTION,        "small-caption"), \
-	MAP(CSS_PROP_VAL_SOFT,                 "soft"), \
-	MAP(CSS_PROP_VAL_SPELL_OUT,            "spell-out"), \
-	MAP(CSS_PROP_VAL_SQUARE,               "square"), \
-	MAP(CSS_PROP_VAL_S_RESIZE,             "s-resize"), \
-	MAP(CSS_PROP_VAL_STATIC,               "static"), \
-	MAP(CSS_PROP_VAL_STATUS_BAR,           "status-bar"), \
-	MAP(CSS_PROP_VAL_SUB,                  "sub"), \
-	MAP(CSS_PROP_VAL_SUPER,                "super"), \
-	MAP(CSS_PROP_VAL_SW_RESIZE,            "sw-resize"), \
-	MAP(CSS_PROP_VAL_TABLE,                "table"), \
-	MAP(CSS_PROP_VAL_TABLE_CAPTION,        "table-caption"), \
-	MAP(CSS_PROP_VAL_TABLE_CELL,           "table-cell"), \
-	MAP(CSS_PROP_VAL_TABLE_COLUMN,         "table-column"), \
-	MAP(CSS_PROP_VAL_TABLE_COLUMN_GROUP,   "table-column-group"), \
-	MAP(CSS_PROP_VAL_TABLE_FOOTER_GROUP,   "table-footer-group"), \
-	MAP(CSS_PROP_VAL_TABLE_HEADER_GROUP,   "table-header-group"), \
-	MAP(CSS_PROP_VAL_TABLE_ROW,            "table-row"), \
-	MAP(CSS_PROP_VAL_TABLE_ROW_GROUP,      "table-row-group"), \
-	MAP(CSS_PROP_VAL_TEXT,                 "text"), \
-	MAP(CSS_PROP_VAL_TEXT_BOTTOM,          "text-bottom"), \
-	MAP(CSS_PROP_VAL_TEXT_TOP,             "text-top"), \
-	MAP(CSS_PROP_VAL_TOP,                  "top"), \
-	MAP(CSS_PROP_VAL_TRANSPARENT,          "transparent"), \
-	MAP(CSS_PROP_VAL_UNDERLINE,            "underline"), \
-	MAP(CSS_PROP_VAL_UPPER_ALPHA,          "upper-alpha"), \
-	MAP(CSS_PROP_VAL_UPPERCASE,            "uppercase"), \
-	MAP(CSS_PROP_VAL_UPPER_LATIN,          "upper-latin"), \
-	MAP(CSS_PROP_VAL_UPPER_ROMAN,          "upper-roman"), \
-	MAP(CSS_PROP_VAL_VISIBLE,              "visible"), \
-	MAP(CSS_PROP_VAL_WAIT,                 "wait"), \
-	MAP(CSS_PROP_VAL_W_RESIZE,             "w-resize"), \
-	MAP(CSS_PROP_VAL_X_FAST,               "x-fast"), \
-	MAP(CSS_PROP_VAL_X_HIGH,               "x-high"), \
-	MAP(CSS_PROP_VAL_X_LOUD,               "x-loud"), \
-	MAP(CSS_PROP_VAL_X_LOW,                "x-low"), \
-	MAP(CSS_PROP_VAL_X_SLOW,               "x-slow"), \
-	MAP(CSS_PROP_VAL_X_SOFT,               "x-soft"), \
-}
+#define CSS_PROP_VAL_MAP(OP, ...) \
+	OP(CSS_PROP_VAL_100,                  "100") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_200,                  "200") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_300,                  "300") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_400,                  "400") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_500,                  "500") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_600,                  "600") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_700,                  "700") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_800,                  "800") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_900,                  "900") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_ABOVE,                "above") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_ABSOLUTE,             "absolute") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_ALWAYS,               "always") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_ARMENIAN,             "armenian") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_AUTO,                 "auto") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_AVOID,                "avoid") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_BASELINE,             "baseline") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_BEHIND,               "behind") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_BELOW,                "below") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_BIDI_OVERRIDE,        "bidi-override") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_BLINK,                "blink") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_BLOCK,                "block") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_BOLD,                 "bold") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_BOLDER,               "bolder") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_BOTH,                 "both") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_BOTTOM,               "bottom") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_CAPITALIZE,           "capitalize") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_CAPTION,              "caption") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_CENTER,               "center") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_CENTER_LEFT,          "center-left") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_CENTER_RIGHT,         "center-right") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_CIRCLE,               "circle") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_CLOSE_QUOTE,          "close-quote") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_CODE,                 "code") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_COLLAPSE,             "collapse") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_CONTINUOUS,           "continuous") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_CROSSHAIR,            "crosshair") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_DECIMAL,              "decimal") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_DECIMAL_LEADING_ZERO, "decimal-leading-zero") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_DEFAULT,              "default") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_DIGITS,               "digits") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_DISC,                 "disc") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_EMBED,                "embed") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_E_RESIZE,             "e-resize") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_FAR_LEFT,             "far-left") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_FAR_RIGHT,            "far-right") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_FAST,                 "fast") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_FASTER,               "faster") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_FIXED,                "fixed") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_GEORGIAN,             "georgian") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_HELP,                 "help") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_HIDDEN,               "hidden") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_HIDE,                 "hide") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_HIGH,                 "high") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_HIGHER,               "higher") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_ICON,                 "icon") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_INHERIT,              "inherit") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_INLINE,               "inline") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_INLINE_BLOCK,         "inline-block") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_INLINE_TABLE,         "inline-table") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_INSIDE,               "inside") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_INVERT,               "invert") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_ITALIC,               "italic") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_JUSTIFY,              "justify") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LEFT,                 "left") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LEFT_SIDE,            "left-side") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LEFTWARDS,            "leftwards") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LEVEL,                "level") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LIGHTER,              "lighter") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LINE_THROUGH,         "line-through") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LIST_ITEM,            "list-item") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LOUD,                 "loud") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LOW,                  "low") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LOWER,                "lower") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LOWER_ALPHA,          "lower-alpha") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LOWERCASE,            "lowercase") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LOWER_GREEK,          "lower-greek") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LOWER_LATIN,          "lower-latin") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LOWER_ROMAN,          "lower-roman") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_LTR,                  "ltr") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_MEDIUM,               "medium") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_MENU,                 "menu") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_MESSAGE_BOX,          "message-box") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_MIDDLE,               "middle") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_MIX,                  "mix") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_MOVE,                 "move") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_NE_RESIZE,            "ne-resize") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_NO_CLOSE_QUOTE,       "no-close-quote") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_NONE,                 "none") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_NO_OPEN_QUOTE,        "no-open-quote") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_NO_REPEAT,            "no-repeat") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_NORMAL,               "normal") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_NOWRAP,               "nowrap") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_N_RESIZE,             "n-resize") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_NW_RESIZE,            "nw-resize") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_OBLIQUE,              "oblique") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_ONCE,                 "once") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_OPEN_QUOTE,           "open-quote") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_OUTSIDE,              "outside") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_OVERLINE,             "overline") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_POINTER,              "pointer") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_PRE,                  "pre") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_PRE_LINE,             "pre-line") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_PRE_WRAP,             "pre-wrap") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_PROGRESS,             "progress") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_RELATIVE,             "relative") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_REPEAT,               "repeat") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_REPEAT_X,             "repeat-x") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_REPEAT_Y,             "repeat-y") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_RIGHT,                "right") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_RIGHT_SIDE,           "right-side") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_RIGHTWARDS,           "rightwards") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_RTL,                  "rtl") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_SCROLL,               "scroll") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_SEPARATE,             "separate") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_SE_RESIZE,            "se-resize") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_SHOW,                 "show") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_SILENT,               "silent") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_SLOW,                 "slow") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_SLOWER,               "slower") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_SMALL_CAPS,           "small-caps") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_SMALL_CAPTION,        "small-caption") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_SOFT,                 "soft") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_SPELL_OUT,            "spell-out") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_SQUARE,               "square") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_S_RESIZE,             "s-resize") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_STATIC,               "static") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_STATUS_BAR,           "status-bar") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_SUB,                  "sub") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_SUPER,                "super") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_SW_RESIZE,            "sw-resize") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_TABLE,                "table") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_TABLE_CAPTION,        "table-caption") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_TABLE_CELL,           "table-cell") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_TABLE_COLUMN,         "table-column") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_TABLE_COLUMN_GROUP,   "table-column-group") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_TABLE_FOOTER_GROUP,   "table-footer-group") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_TABLE_HEADER_GROUP,   "table-header-group") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_TABLE_ROW,            "table-row") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_TABLE_ROW_GROUP,      "table-row-group") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_TEXT,                 "text") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_TEXT_BOTTOM,          "text-bottom") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_TEXT_TOP,             "text-top") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_TOP,                  "top") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_TRANSPARENT,          "transparent") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_UNDERLINE,            "underline") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_UPPER_ALPHA,          "upper-alpha") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_UPPERCASE,            "uppercase") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_UPPER_LATIN,          "upper-latin") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_UPPER_ROMAN,          "upper-roman") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_VISIBLE,              "visible") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_WAIT,                 "wait") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_W_RESIZE,             "w-resize") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_X_FAST,               "x-fast") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_X_HIGH,               "x-high") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_X_LOUD,               "x-loud") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_X_LOW,                "x-low") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_X_SLOW,               "x-slow") __VA_ARGS__ \
+	OP(CSS_PROP_VAL_X_SOFT,               "x-soft") __VA_ARGS__
