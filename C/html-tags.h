3f1be401fee801293281ed957ab566d3719c5abb
technosaurus <technosaurus@yahoo.com>
2022-09-07 18:12:21 -0700

    Add some html5 tags

--- C/html-tags.h
+++ C/html-tags.h
@@ -1,3 +1,8 @@
+#ifdef REMOVE_OBSOLETE_TAGS
+#define OBSOLETE(...)
+#else
+#define OBSOLETE(...) __VA_ARGS__
+#endif
 #define HTML_TAGMAP(OP, ...) \
 	/* ENUM/TAG    ... DEFAULT CSS PROPERTIES ... */ \
 	OP(!--,       /* */) __VA_ARGS__ \
@@ -28,6 +33,7 @@
 	OP(code,      "font-family:monospace") __VA_ARGS__ \
 	OP(col,       "display:table-column") __VA_ARGS__ \
 	OP(colgroup,  "display:table-column-group") __VA_ARGS__ \
+	OP(data,  /* */) __VA_ARGS__ \
 	OP(datalist,  /* */) __VA_ARGS__ \
 	OP(dd,        "display:block","margin-left:40px","unicode-bidi:embed") __VA_ARGS__ \
 	OP(del,       "text-decoration:line-through") __VA_ARGS__ \
@@ -56,6 +62,7 @@
 	OP(h6,        "display:block","font-size: .75em","font-weight:bolder","margin: 1.67em 0","unicode-bidi:embed") __VA_ARGS__ \
 	OP(head,      "display:none") __VA_ARGS__ \
 	OP(header,    /* */) __VA_ARGS__ \
+	OP(hgroup,    /* */) __VA_ARGS__ \
 	OP(hr,        "border:1px inset","display:block","unicode-bidi:embed") __VA_ARGS__ \
 	OP(html,      "display:block","unicode-bidi:embed") __VA_ARGS__ \
 	OP(i,         "font-style: italic") __VA_ARGS__ \
@@ -64,6 +71,7 @@
 	OP(input,     "display:inline-block") __VA_ARGS__ \
 	OP(ins,       "text-decoration:underline") __VA_ARGS__ \
 	OP(kbd,       "font-family:monospace") __VA_ARGS__ \
+	OP(keygen,      /* */) __VA_ARGS__ \
 	OP(label,     /* */) __VA_ARGS__ \
 	OP(legend,    /* */) __VA_ARGS__ \
 	OP(li,        "display:list-item") __VA_ARGS__ \
@@ -94,6 +102,7 @@
 	OP(ruby,      /* */) __VA_ARGS__ \
 	OP(s,         "text-decoration:line-through") __VA_ARGS__ \
 	OP(samp,      "font-family:monospace") __VA_ARGS__ \
+	OP(section,   /* */) __VA_ARGS__ \
 	OP(select,    "display:inline-block") __VA_ARGS__ \
 	OP(small,     "font-size:.83em") __VA_ARGS__ \
 	OP(source,    /* */) __VA_ARGS__ \
